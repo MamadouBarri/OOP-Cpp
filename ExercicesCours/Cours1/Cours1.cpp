4efda4b813fbc2f37cf2e68f61eed41eda5ec0a4
Mamadou <mamadou236@gmail.com>
2020-01-15 11:21:22 -0500

    cours 2

--- ExercicesCours/Cours1/Cours1.cpp
+++ ExercicesCours/Cours1/Cours1.cpp
@@ -1,39 +1,101 @@
 // Cours1.cpp 
 #include <iostream>
+#include <memory>
+#include <algorithm>
 
-
+using namespace std;
 class Matrice {
 public:
     Matrice(int hauteur = 0, int largeur = 0);
-    ~Matrice();
+    const double& acceder(int ligne, int colonne) const;
     double& acceder(int ligne, int colonne);
+        //getters
+    int getHauteur() const;
+    int getLargeur() const;
+    void changerDimensions(int hauteur, int largeur);
 private:
+    double& accederInterne(int ligne, int colonne) const;
     int hauteur_;
     int largeur_;
-    double* valeurs_;
+    //lorsque l'objet est const on peut modifier les valeurs pointe mais pas le pointeur 
+    unique_ptr<double[]> valeurs_;//
 };
 
-Matrice::Matrice(int hauteur, int largeur) {
-     hauteur_ = hauteur;
-     largeur_ = largeur;
-     valeurs_ = new double[hauteur_ * largeur_];
+void Matrice::changerDimensions(int hauteur, int largeur) 
+{
+    Matrice copie(hauteur, largeur);
+    for (int i = 0; i < min(hauteur, hauteur_); i++)
+        for (int j = 0; j < min(largeur, largeur_); j++)
+            copie.acceder(i, j) = acceder(i, j);
+    hauteur_ = hauteur;
+    largeur_ = largeur;
+    valeurs_ = move(copie.valeurs_);
+    //la copie est detrurie a la fin de laccolade car variable locale
 }
 
-double& Matrice::acceder(int ligne, int colonne) {
+int Matrice::getHauteur() const {
+    return hauteur_;
+}
+
+int Matrice::getLargeur() const {
+    return largeur_;
+}
+
+Matrice::Matrice(int hauteur, int largeur) : 
+    hauteur_(hauteur),
+    largeur_(largeur),
+    valeurs_(make_unique<double[]>(hauteur_ * largeur_))
+{
+}
+
+double& Matrice::accederInterne(int ligne, int colonne) const {
     return valeurs_[ligne * largeur_ + colonne];
 }
 
-Matrice::~Matrice() {
-    delete[] valeurs_;
+const double& Matrice::acceder(int ligne, int colonne) const {
+    return accederInterne(ligne, colonne);
 }
-using namespace std;
+
+double& Matrice::acceder(int ligne, int colonne) {
+    return accederInterne(ligne, colonne);
+}
+
+void afficherValeurMatrice(const Matrice& matrice) {
+        cout << matrice.acceder(2, 5) << endl;
+}
+
+void modifierMatrice(Matrice& matrice) {
+    matrice.acceder(2, 5) +=10;
+    //puisque cest une copie qui se termine dans le bloc -> le destructeur est appele pour matrice donc desalloue
+}
+
+void calculer(Matrice matrice) {
+    matrice.acceder(2, 5) += 100;
+    cout << matrice.acceder(2, 5) << endl;
+}
+
+Matrice copierMatrice(const Matrice& matrice){
+    Matrice copie(matrice.getHauteur(), matrice.getLargeur());
+    for (int i = 0; i < copie.getHauteur(); i++) {
+        for (int j = 0; j < copie.getLargeur(); j++) {
+            copie.acceder(i, j) = matrice.acceder(i, j);
+        }
+    }
+    return copie;
+}
+
 int main()
 {
     Matrice matrice(20,30);
-    Matrice m;
-    matrice.acceder(2, 5) = 5;
+    matrice.acceder(2, 5) = 4;
     matrice.acceder(3, 2) = 120;
+    modifierMatrice(matrice);
+    afficherValeurMatrice(matrice);
+    //un move implicite est fait a chaque fois puisquon ne va pas lacceder une autre fois : return et passage par parametres sont des move explicite meme avec un shared pointer -- > depend pas du type
+    calculer(copierMatrice(matrice));
     cout << matrice.acceder(2, 5 ) << endl;
+    matrice.changerDimensions(5, 2);
     cout << matrice.acceder(3, 2) << endl;
+    //ici le destructeur est encore appelle et donc on desalloue de la memoire deja desalloue
 }
 
